491eaaf21c41f4fb4f11a3665b56eea56a1bee95
Sasha Rosenbaum <divineops@github.com>
2020-05-04 15:48:21 -0500

    Update bashprompt.sh

--- bashprompt.sh
+++ bashprompt.sh
@@ -35,7 +35,7 @@ check_branch() {
 export PS1="\[\e[0;32m\]➜ \[\e[0m\]\[\$(check_status)\]"
        PS1+="\[$(tput sgr0)\]\[\e[1;34m\]\W\[\e[0m\]"
        PS1+="\[\e[0;36m\]\$(check_branch)\[\e[0m\]\n    \[$(tput sgr0)\]"
-       PS1+="\[\e[0;33m\] divineops:\[\e[0m\]\[$(tput sgr0)\]\[\e[1;37m\] "
+       PS1+="\[\e[0;35m\] divineops:\[\e[0m\]\[$(tput sgr0)\]\[\e[1;37m\] "
 
 export CLICOLOR=1
 export LSCOLORS=fxfxBxDxgxegedabagacad

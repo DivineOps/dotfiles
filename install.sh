2a37362247ae4ddce518dd043ca5cd9d4a3e3614
Sasha Rosenbaum <sasha@sasharosenbaum.com>
2021-01-02 09:00:27 -0600

    add gitconfig and symlink lines

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+#Install for Codespaces
 set -x
 
 cd ~/
 
 echo "" >> ~/.bashrc
 echo ". ~/dotfiles/bashprompt.sh" >> ~/.bashrc
+
+
+#symlinks for Mac
+ln -sv "$PWD/git/.gitconfig" ~
+
+ln -sv "$PWD/bashprompt.sh" ~
